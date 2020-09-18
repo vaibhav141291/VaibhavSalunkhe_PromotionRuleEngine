a97b141719feb6b388c6d2bbf4f0438845f5dd6d
Vaibhav Salunkhe <vaibhav.salunkhe04@gmail.com>
2020-09-18 11:53:51 +0530

    First Testcase (scenario) added and executed successfully

--- PromotionRulesEngineTest/UnitTest1.cs
+++ PromotionRulesEngineTest/UnitTest1.cs
@@ -12,36 +12,30 @@ namespace PromotionRulesEngineTest
     {
         PromotionRepository promotionRepository = new PromotionRepository();
         Product product = new Product();
-        Product expectedResult = new Product();
+        List<Product> expectedResult = new List<Product>();
 
+
+        /// <summary>
+        /// no discount/promo will be applicable in this scenario
+        /// </summary>
         [TestMethod]
         public void Scenario1()
         {
-            product = new Product()
-            {
-                ProductCode = "A",
-                Quantity = 5,
-                Amount = 250
-            };
-
             List<Product> Cart = new List<Product>();
+            product = new Product() { ProductCode = "A", Quantity = 1, Amount = 50 };
             Cart.Add(product);
-
-            expectedResult = new Product()
-            {
-                ProductCode = product.ProductCode,
-                Quantity = product.Quantity,
-                Amount = product.Amount,
-                IsPromoApplied = true,
-                Discount = 20
-            };
+            product = new Product() { ProductCode = "b", Quantity = 1, Amount = 30};
+            Cart.Add(product);
+            product = new Product() { ProductCode = "c", Quantity = 1, Amount = 20 };
+            Cart.Add(product);
+            expectedResult = Cart;
 
             Cart = promotionRepository.ApplyPromoToProductA(ref Cart);
+            Cart = promotionRepository.ApplyPromoToProductB(ref Cart);
+            Cart = promotionRepository.ApplyPromoToProductC(ref Cart);
+            Cart = promotionRepository.ApplyPromoToProductD(ref Cart);
 
-            Product result = Cart.Where(x => x.IsPromoApplied).FirstOrDefault();
-
-            Assert.ReferenceEquals(result, expectedResult);
-
+            Assert.ReferenceEquals(Cart, expectedResult);
         }
 
 
