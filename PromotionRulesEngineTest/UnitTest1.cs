51032183db5cccf651f0a37513d6d538c78f46dc
Vaibhav Salunkhe <vaibhav.salunkhe04@gmail.com>
2020-09-18 12:24:25 +0530

    All test cases added and tested successfully

--- PromotionRulesEngineTest/UnitTest1.cs
+++ PromotionRulesEngineTest/UnitTest1.cs
@@ -35,11 +35,11 @@ namespace PromotionRulesEngineTest
             Cart = promotionRepository.ApplyPromoToProductC(ref Cart);
             Cart = promotionRepository.ApplyPromoToProductD(ref Cart);
 
-            expectedResult = 0;
-            int result = 0;
+            expectedResult = 100;
+            int result = Cart.Sum(x => x.Amount);
             if (Cart.Any(x => x.Discount > 0))
             {
-                result = Cart.Where(x => x.IsPromoApplied).FirstOrDefault().Discount;
+                result = Cart.Sum(x => x.Amount) - Cart.Where(x => x.IsPromoApplied).FirstOrDefault().Discount;
             }
 
             Assert.AreEqual(expectedResult, result);
@@ -56,17 +56,17 @@ namespace PromotionRulesEngineTest
             Cart.Add(product);
             product = new Product() { ProductCode = "c", Quantity = 1, Amount = 20 };
             Cart.Add(product);
-            
+
             Cart = promotionRepository.ApplyPromoToProductA(ref Cart);
             Cart = promotionRepository.ApplyPromoToProductB(ref Cart);
             Cart = promotionRepository.ApplyPromoToProductC(ref Cart);
             Cart = promotionRepository.ApplyPromoToProductD(ref Cart);
 
-            expectedResult = 30;
-            int result = 0;
+            expectedResult = 390;
+            int result = Cart.Sum(x => x.Amount);
             if (Cart.Any(x => x.Discount > 0))
             {
-                result = Cart.Where(x => x.IsPromoApplied).FirstOrDefault().Discount;
+                result = Cart.Sum(x => x.Amount) - Cart.Where(x => x.IsPromoApplied).FirstOrDefault().Discount;
             }
 
             Assert.AreEqual(expectedResult, result);
@@ -74,11 +74,33 @@ namespace PromotionRulesEngineTest
 
 
         [TestMethod]
-        public void Scenario3() {
-        
+        public void Scenario3()
+        {
+            List<Product> Cart = new List<Product>();
+            product = new Product() { ProductCode = "A", Quantity = 3, Amount = 150 };// promo will give 20 discount
+            Cart.Add(product);
+            product = new Product() { ProductCode = "b", Quantity = 5, Amount = 150 }; // promo will give 30 discount
+            Cart.Add(product);
+            product = new Product() { ProductCode = "c", Quantity = 1, Amount = 20 };
+            Cart.Add(product);
+            product = new Product() { ProductCode = "D", Quantity = 1, Amount = 15 };// Promo will give 5  discount, but will not be considered because productB discount is higher
+            Cart.Add(product);
+
+
+            Cart = promotionRepository.ApplyPromoToProductA(ref Cart);
+            Cart = promotionRepository.ApplyPromoToProductB(ref Cart);
+            Cart = promotionRepository.ApplyPromoToProductC(ref Cart);
+            Cart = promotionRepository.ApplyPromoToProductD(ref Cart);
+
+            expectedResult = 305;
+            int result = Cart.Sum(x => x.Amount);
+            if (Cart.Any(x => x.Discount > 0))
+            {
+                result = Cart.Sum(x => x.Amount) - Cart.Where(x => x.IsPromoApplied).FirstOrDefault().Discount;
+            }
+
+            Assert.AreEqual(expectedResult, result);
         }
 
-        [TestMethod]
-        public void Scenario4() { }
     }
 }
